859d29e7ef1dae364995d422e6bb143fa5f9b9d9
vnnikolov30 <v.n.nikolov30@gmail.com>
2024-09-26 17:14:16 +0300

    Initial commit
    set up react + tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <>
+      <p>
+        I’m Derek, an astro-engineer based in Tattooine. I like to build X-Wings
+        at
+        <a class="underline decoration-sky-500">My Company, Inc</a>. Outside of
+        work, I like to{" "}
+        <a class="underline decoration-pink-500">watch pod-racing</a> and have{" "}
+        <a class="underline decoration-indigo-500">light-saber</a> fights.
+      </p>
+    </>
+  );
+}
+
+export default App;
